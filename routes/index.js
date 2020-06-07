cc7a8ff9d3b4379cb17291cd4e66a987b65155be
jonhassall <jon@jonhassall.com>
2020-06-07 16:30:34 +0100

    Remove route to about page, and also serve on subdirectory

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get(['/', '/mousetracker'], function(req, res, next) {
   res.render('index', { title: 'Realtime Cursor Tracking' });
-});
-
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About' });
 });
 
 module.exports = router;
