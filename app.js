65f1e5ca2acc9c9e04312105c7a7e4ca1ce34f51
jonhassall <jon@jonhassall.com>
2020-06-07 16:35:59 +0100

    Static files alias

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(sassMiddleware({
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/mousetracker', express.static('public'))
+app.use('/mousetracker', express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
